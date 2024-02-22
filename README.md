ae68498e4de9c0dcaf99ca0eca592d1b40ee77ba
mayankchugh1977 <mayank.chugh@gmail.com>
2024-02-22 14:34:25 +0800

    folder structure

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # US-Visa-Approval-Prediction
-US-Visa-Approval-Prediction
+
+## Git commands
+
+```bash
+git add .
+
+git commit -m "Updated"
+
+git push origin main
+```
+
+## How to run?
+
+```bash
+conda create -n visa python=3.8 -y
+```
+
+```bash
+conda activate visa
+```
+
+```bash
+pip install -r requirements.txt
+```
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Mayank Chugh",
+    author_email="mayankchugh.learning@gmail.com",
+    description="US Visa Project",
+    long_description="US Visa Project",
+    long_description_content_type="text/markdown",
+    url="https://github.com/mayankchugh/us_visa",
+    packages=find_packages()
+)
